426eeabf31e7f036103229562cbab3f83ef12d75
champjj <champ.j@outlook.com>
2022-12-19 16:12:03 +0700

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f14ebafa603da73b38ac530537ff7b17'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c5b8d118-1fa7-4016-a06e-3d0f30443639'
+};
